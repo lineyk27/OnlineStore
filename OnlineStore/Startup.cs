57c3f2be9d769e424c1b33eee87e5e21bc3a04d7
lineyk27 <lineyk27gg@gmail.com>
2019-12-15 16:08:16 +0200

    Added connecting to database

--- OnlineStore/Startup.cs
+++ OnlineStore/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+using OnlineStore.Data;
 
 namespace OnlineStore
 {
@@ -24,6 +27,10 @@ namespace OnlineStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<OnlineStoreDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("MSSqlServer"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
