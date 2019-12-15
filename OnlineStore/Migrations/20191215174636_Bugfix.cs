09401afba001064e97e80196e22cce81741805d9
lineyk27 <lineyk27gg@gmail.com>
2019-12-15 20:15:07 +0200

    Bugfix

---
+++ OnlineStore/Migrations/20191215174636_Bugfix.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStore.Migrations
+{
+    public partial class Bugfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: -1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { -1, "Administrator" });
+        }
+    }
+}
