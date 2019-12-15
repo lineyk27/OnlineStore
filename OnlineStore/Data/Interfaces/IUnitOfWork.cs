5c64b26ae7c121230071b91dc5eed0a356e47009
lineyk27 <lineyk27gg@gmail.com>
2019-12-15 15:12:58 +0200

    Added unitofwork pattern for data access

---
+++ OnlineStore/Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using OnlineStore.Models.Database;
+
+namespace OnlineStore.Data.Interfaces
+{
+    interface IUnitOfWork
+    {
+        Repository<Category> CategoryRepository { get; set; }
+        Repository<Comment> CommentRepository { get; set; }
+        Repository<Product> ProductRepository { get; set; }
+        Repository<Purchase> PurchaseRepository{ get; set; }
+        Repository<PurchaseProduct> PurchaseProductRepository{ get; set; }
+        Repository<Rate> RateRepository{ get; set; }
+        Repository<ShoppingCart> ShoppingCartRepository{ get; set; }
+        Repository<User> UserRepository{ get; set; }
+        Repository<UserRole> UserRoleRepository{ get; set; }
+    }
+}
