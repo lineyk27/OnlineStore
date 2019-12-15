44d636f5af6e6d4fa5ce02863753658abe0c880a
lineyk27 <lineyk27gg@gmail.com>
2019-12-15 15:03:18 +0200

    Added data access repository pattern

---
+++ OnlineStore/Data/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace OnlineStore.Data.Interfaces
+{
+    interface IRepository<TEntity> 
+        where TEntity: class
+    {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+                                Func<IQueryable<TEntity>,
+                                IOrderedQueryable<TEntity>> orderBy = null,
+                                string includeProperties = "");
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        void Update(TEntity entityToUpdate);
+    }
+}
---
+++ OnlineStore/Data/Repository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Data.Interfaces;
+
+namespace OnlineStore.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        internal OnlineStoreDbContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public Repository(OnlineStoreDbContext cont)
+        {
+            context = cont;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get(System.Linq.Expressions.Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (var includeProperty in includeProperties.Split
+                    (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public TEntity GetByID(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
